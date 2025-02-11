65c3d128b9d2f2f570eb900c7ea5ae3a4a5f33f0
Brian Caldera <briancaldera07@gmail.com>
2025-02-11 02:49:44 -0400

    ⚙️ feat: add working implementation for API
    -Update requirements.txt
    -Add .env
    -Add CA file
    -Add DB connection
    -Basic project structure

--- functions/main.py
+++ functions/main.py
@@ -1,5 +1,52 @@
-from firebase_functions import https_fn, options
+import locale
+import os
+from datetime import datetime, timedelta
 
-@https_fn.on_request(cors=True)
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+from firebase_admin import initialize_app, functions
+from firebase_functions import https_fn, options, tasks_fn
+from firebase_functions.options import RetryConfig
+from playhouse.db_url import connect
+
+from db import save_to_db
+from models.Currency import Currency
+from source import get_source
+from utils import get_function_url
+
+locale_string: str = os.getenv("LOCALE")
+
+locale.setlocale(locale.LC_ALL, locale_string)
+
+initialize_app()
+
+
+@https_fn.on_request(cors=options.CorsOptions(cors_origins="*", cors_methods=["get"]))
+def update_currencies(req: https_fn.Request):
+    currencies: dict = get_source()
+    save_to_db(currencies)
+    return https_fn.Response(status=200, response="OK")
+
+
+@https_fn.on_request()
+def enqueue_initialize_db(_: https_fn.Request) -> https_fn.Response:
+    task_queue = functions.task_queue("initialize_db")
+    target_uri = get_function_url("initialize_db")
+
+    start_time = datetime.now() + timedelta(seconds=10)
+
+    body = {"data": {"date": start_time.isoformat()[:10]}}
+
+    task_options = functions.TaskOptions(schedule_time=start_time, uri=target_uri)
+
+    task_queue.enqueue(body, task_options)
+    return https_fn.Response(status=200, response="Enqueued")
+
+
+@tasks_fn.on_task_dispatched(
+    retry_config=RetryConfig(max_attempts=0),
+)
+def initialize_db(req: tasks_fn.CallableRequest) -> bool:
+    database_url = os.getenv("DATABASE_URL")
+    db = connect(database_url)
+    db.connect()
+    db.create_tables([Currency])
+    return db.close()
---
+++ functions/source.py
@@ -0,0 +1,52 @@
+import os
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+
+def get_source() -> dict:
+    url = os.getenv("SOURCE_URL")
+    page = urlopen(url, cafile="../assets/_.bcv.org.ve.crt")
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+
+    dollar = (
+        soup.css.select("#dolar > div > div > div.col-sm-6.col-xs-6.centrado > strong")[
+            0
+        ].string
+        or None
+    )
+    euro = (
+        soup.css.select("#euro > div > div > div.col-sm-6.col-xs-6.centrado > strong")[
+            0
+        ].string
+        or None
+    )
+    ruble = (
+        soup.css.select("#rublo > div > div > div.col-sm-6.col-xs-6.centrado > strong")[
+            0
+        ].string
+        or None
+    )
+    yuan = (
+        soup.css.select("#yuan > div > div > div.col-sm-6.col-xs-6.centrado > strong")[
+            0
+        ].string
+        or None
+    )
+    lira = (
+        soup.css.select("#lira > div > div > div.col-sm-6.col-xs-6.centrado > strong")[
+            0
+        ].string
+        or None
+    )
+
+    currencies = {
+        "USD": dollar,
+        "EUR": euro,
+        "RUB": ruble,
+        "CNY": yuan,
+        "TRY": lira,
+    }
+
+    return currencies
