48bdb54a8dae70a648cae90b97d5316974e30243
Brian Caldera <briancaldera07@gmail.com>
2025-05-13 17:49:08 -0400

    🔧 fix: change members visibility

--- functions/exchange_rate/adapter/output/database/connection.py
+++ functions/exchange_rate/adapter/output/database/connection.py
@@ -3,21 +3,21 @@ from flask import Flask
 from playhouse.flask_utils import FlaskDB
 from playhouse.postgres_ext import PostgresqlExtDatabase
 
-db_host = os.getenv("DB_HOST")
-db_port = os.getenv("DB_PORT")
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-db_name = os.getenv("DB_NAME")
+_db_host = os.getenv("DB_HOST")
+_db_port = os.getenv("DB_PORT")
+_db_user = os.getenv("DB_USER")
+_db_password = os.getenv("DB_PASSWORD")
+_db_name = os.getenv("DB_NAME")
 
-supabase_database = PostgresqlExtDatabase(
-    db_name,
-    host=db_host,
-    user=db_user,
-    password=db_password,
-    port=db_port,
+_supabase_database = PostgresqlExtDatabase(
+    _db_name,
+    host=_db_host,
+    user=_db_user,
+    password=_db_password,
+    port=_db_port,
 )
 
 app = Flask("tubolivarhoy")
 app.config.from_object("tubolivarhoy")
 
-db_wrapper = FlaskDB(app, supabase_database)
+db_wrapper = FlaskDB(app, _supabase_database)
--- functions/exchange_rate/domain/value_objects/__init__.py
+++ functions/exchange_rate/domain/value_objects/__init__.py
@@ -1,3 +1,5 @@
 from .exchange_id import ExchangeID
 from .rate import Rate
 from .currency import Currency
+
+__all__ = ["ExchangeID", "Rate", "Currency"]
