7e0c9dff51a4bef2d9c255a0ebbebcce584fa9c2
Brian Caldera <briancaldera07@gmail.com>
2026-02-24 17:26:01 -0400

    ✨ add check integrity

--- functions-ts/src/exchange-rate/functions.ts
+++ functions-ts/src/exchange-rate/functions.ts
@@ -1,15 +1,17 @@
 import * as logger from 'firebase-functions/logger'
 import * as cheerio from 'cheerio'
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
-import { Database } from './database.types'
+import { Database } from '../../generated/database.types'
 import {
   Currency,
   CurrencyValues,
   ExchangeRate,
 } from './domain/models/exchange-rate'
-import {getCustomAgent} from "./utils/custom-fetch";
+import { getCustomAgent } from './utils/custom-fetch'
 import fetch from 'node-fetch'
-import {z} from 'zod'
+import { z } from 'zod'
+import { compareDesc, isBefore } from 'date-fns'
+import { IntegrityService } from './application/integrity-service'
 
 const TARGET_URL = 'https://www.bcv.org.ve/'
 
@@ -23,11 +25,10 @@ const selectors: Record<Currency, string> = {
 
 export async function fetchExchangeRates(): Promise<void> {
   try {
-
     const agent = await getCustomAgent()
 
     const res = await fetch(TARGET_URL, {
-      agent: agent
+      agent: agent,
     })
 
     const html = await res.text()
@@ -95,7 +96,6 @@ function currencyStringToNumber(rate: string): number {
 async function saveExchangeRates(
   rates: Record<Currency, number | null>,
 ): Promise<void> {
-
   const schema = z.record(z.enum(CurrencyValues), z.number())
 
   const validData = schema.parse(rates)
@@ -109,7 +109,7 @@ async function saveExchangeRates(
 
   const { count } = await supabaseClient
     .from('exchange_rates')
-    .select('*', { count: 'exact' })
+    .select('*', { count: 'exact', head: true })
     .eq('registered_at', now.toISOString())
 
   if (count && count > 0) {
@@ -126,20 +126,106 @@ async function saveExchangeRates(
 
   logger.info('Saving exchange rates...')
   const exchangeRateData = exchangeRates.map((rate) => rate.toJSON())
-  await supabaseClient.from('exchange_rates').insert(exchangeRateData)
+  const { error } = await supabaseClient
+    .schema('private')
+    .from('exchange_rates')
+    .insert(exchangeRateData)
 
-  logger.info('Exchange rates saved successfully')
+  if (error) {
+    logger.error('Error saving exchange rates...')
+    logger.error(error)
+  } else {
+    logger.info('Exchange rates saved successfully')
+  }
 }
 
-let client: SupabaseClient
+let client: SupabaseClient<Database>
 
 function createSupabaseClient(): SupabaseClient<Database> {
+  const SUPABASE_URL = z.string().nonempty().parse(process.env.SUPABASE_URL)
+  const SUPABASE_SECRET_KEY = z
+    .string()
+    .nonempty()
+    .parse(process.env.SUPABASE_SECRET_KEY)
+
   if (!client) {
-    client = createClient<Database>(
-      process.env.SUPABASE_URL ?? '',
-      process.env.SUPABASE_SECRET_KEY ?? '',
-    )
+    client = createClient<Database>(SUPABASE_URL, SUPABASE_SECRET_KEY, {
+      db: {
+        schema: 'private',
+      },
+    })
   }
 
   return client
+}
+
+export async function getExchangeRateForPeriod(
+  currency: string,
+  hours: [string, string],
+) {
+  const validCurrency = z.enum(CurrencyValues).parse(currency)
+  const validHours = z
+    .tuple([z.iso.datetime(), z.iso.datetime()])
+    .refine(
+      (value) => isBefore(value[0], value[1]),
+      'Start date must be before end date',
+    )
+    .parse(hours)
+
+  const supabaseClient = createSupabaseClient()
+
+  const { data } = await supabaseClient
+    .from('exchange_rates')
+    .select('*')
+    .eq('currency', validCurrency)
+    .gte('registered_at', validHours[0])
+    .lte('registered_at', validHours[1])
+    .throwOnError()
+
+  const data2 = data
+    .sort((a, b) => {
+      if (!a.registered_at || !b.registered_at) {
+        return 0
+      }
+
+      return compareDesc(new Date(a.registered_at), new Date(b.registered_at))
+    })
+    .map((rate) => {
+      const { currency, id, ...payload } = rate
+
+      return payload
+    })
+
+  if (!hasRateAndDatetime(data2)) {
+    throw new Error('There are rates without datetime or rate')
+  }
+
+  const map = new Map<number, string[]>()
+
+  data2.forEach((rate) => {
+    const set = map.get(rate.rate)
+
+    if (!set) {
+      map.set(rate.rate, [rate.registered_at])
+    } else {
+      map.set(rate.rate, [...set, rate.registered_at])
+    }
+  })
+
+  return map
+}
+
+function hasRateAndDatetime(
+  data: { rate: number | null; registered_at: string | null }[],
+): data is {
+  rate: number
+  registered_at: string
+}[] {
+  return data.every((rate) => rate.rate && rate.registered_at)
+}
+
+export async function checkIntegrity() {
+  const db = createSupabaseClient()
+  const integrityService = new IntegrityService(db)
+  await integrityService.checkIntegrity()
 }
